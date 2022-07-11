4e08f0e20330548b48bc7dd5b6a950d3c2ee796b
rishshub <rishabhrastogi0202@gmail.com>
2022-07-11 21:18:06 +0530

    Rishab

---
+++ script.js
@@ -0,0 +1,11 @@
+var name =document.getElementById("name");
+var roll_no=document.getElementById("Roll_no");
+var college_name =document.getElementById("University");
+var date =document.getElementById("Date");
+var date =new Date();
+window.onload=()=>{
+    name.innerText='Rishabh Rastogi';
+    roll_no.innerText='201500567';
+    college_name.innerText='GLA UNIVERSITY,MATHURA';
+}
+
